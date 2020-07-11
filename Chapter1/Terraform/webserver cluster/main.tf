e307ab5d141574aced94258085c64202921cac44
PMardolker <PMardolker@yahoo.com>
2020-07-11 23:00:11 +0530

    corrected the file

--- Chapter1/Terraform/webserver cluster/main.tf
+++ Chapter1/Terraform/webserver cluster/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_launch_configuration" "example" {
   image_id        = "ami-0c55b159cbfafe1f0"
   instance_type   = "t2.micro"
-  security_groups = [aws_security_group.instance.pwd]
+  security_groups = [aws_security_group.instance.id]
 
   user_data = <<-EOF
               #!/bin/bash

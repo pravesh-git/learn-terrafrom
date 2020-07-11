9264dfee9c2800a07727294f3aef789a4dc789eb
PMardolker <PMardolker@yahoo.com>
2020-07-11 15:12:43 +0530

    set variables and output

--- Chapter1/Terraform/main.tf
+++ Chapter1/Terraform/main.tf
@@ -2,6 +2,13 @@ provider "aws" {
     region = "us-east-2" 
 }
 
+variable "server_port" {
+    description =  "The port the server will use for HTTP requests"
+    type        = number
+    default     = 8080
+}
+
+
 resource "aws_instance" "pravesh_test" {
     ami           = "ami-0c55b159cbfafe1f0"
     instance_type = "t2.micro"
@@ -10,7 +17,7 @@ resource "aws_instance" "pravesh_test" {
     user_data = <<-EOF
                 #!/bin/bash
                 echo "Hello, World" > index.html
-                nohup busybox httpd -f -p 8080 &
+                nohup busybox httpd -f -p ${var.server_port} &
                 EOF 
 
     tags = {
@@ -22,10 +29,15 @@ resource "aws_security_group" "instane" {
     name = "terraform-example-instance"
 
     ingress {
-        from_port  = 8080
-        to_port    = 8080
+        from_port  = var.server_port
+        to_port    = var.server_port
         protocol   = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }  
 
+}
+
+output "pubic_ip" {
+    value = aws_instance.pravesh_test.public_ip
+    description = "The public ip address of the web server"
 }

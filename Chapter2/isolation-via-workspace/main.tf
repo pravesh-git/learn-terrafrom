9ea1c816e1e63342e1fb65b392553e3b62163d70
PMardolker <PMardolker@yahoo.com>
2020-07-12 19:38:51 +0530

    worspace concept

---
+++ Chapter2/isolation-via-workspace/main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+region = "us-east-2"
+}
+
+
+resource "aws_instance" "example" {
+ami = "ami-0c55b159cbfafe1f0"
+instance_type = "t2.micro"
+}
+
+terraform {
+    backend "s3" {
+        # Replace this with your bucket name!
+        bucket = "terraform-up-and-running-state-pravesh"
+        key = "workspaces-example/terraform.tfstate"
+        region = "us-east-2"
+        # Replace this with your DynamoDB table name!
+        dynamodb_table = "terraform-up-and-running-locks"
+        encrypt = true
+    }
+}
